7d1ee01ccfe105000f657d61f65c7701e1aacb92
Lee Riemenschneider <lwriemen@frogooa.com>
2018-02-15 06:49:10 -0500

    job #9955 - Commit of addition of Element On Canvas associative class on R1.

--- utilities/build/build_configuration.sh
+++ utilities/build/build_configuration.sh
@@ -1,12 +1,12 @@
 # This script sets up environment variables that
 # are required to prepare a development workspace
 # as well as build such a workspace
-export BP_GIT_DIR=~/git
-# for mac us this install dir
-export bp_install_dir=~/xtuml/BridgePoint.app/Contents/Eclipse/
-# for linux uncomment this line and comment the mac version
-#export bp_install_dir=~/xtuml/BridgePoint
-export WORKSPACE=~/workspace
+export BP_GIT_DIR=/home/lee/git
+
+export bp_install_dir=/home/lee/devBridgePoint
+
+
+export WORKSPACE=/home/lee/workspace/build
 export INCLUDE_TESTS=false
 export XTUML_DEVELOPMENT_REPOSITORY=${BP_GIT_DIR}/bridgepoint
 export XTUML_TEST_MODEL_REPOSITORY=${BP_GIT_DIR}/models/test
